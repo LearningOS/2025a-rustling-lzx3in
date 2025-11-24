0ada6bee6890964aa85b381b5ed99dbb1c98f88f
lzx3in <lzx3in@qunle.cc>
2025-11-24 20:10:05 +0800

    Complete exercise algorithm8

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -2,7 +2,6 @@
 	queue
 	This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -52,30 +51,43 @@ impl<T> Default for Queue<T> {
     }
 }
 
+#[derive(Debug)]
 pub struct myStack<T>
 {
-	//TODO
 	q1:Queue<T>,
 	q2:Queue<T>
 }
 impl<T> myStack<T> {
     pub fn new() -> Self {
         Self {
-			//TODO
 			q1:Queue::<T>::new(),
 			q2:Queue::<T>::new()
         }
     }
     pub fn push(&mut self, elem: T) {
-        //TODO
+        self.q1.enqueue(elem);
     }
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-		Err("Stack is empty")
+        if self.is_empty() {
+            return Err("Stack is empty");
+        }
+
+        let n = self.q1.size();
+        for i in 0..(n - 1) {
+            let x = self.q1.dequeue().unwrap();
+            self.q2.enqueue(x);
+        }
+
+        let top = self.q1.dequeue().unwrap();
+
+        while let Ok(x) = self.q2.dequeue() {
+            self.q1.enqueue(x);
+        }
+
+        Ok(top)
     }
     pub fn is_empty(&self) -> bool {
-		//TODO
-        true
+        self.q1.is_empty()
     }
 }
 
